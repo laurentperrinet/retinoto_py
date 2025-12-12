0c54664bb10b1c98b40eeef2c48473745c004774
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2025-12-12 06:55:30 -0500

    Add multiprocessing fork start method in package init
    Force the start method to 'fork' to ensure consistent process behavior across platforms.

--- src/retinoto_py/__init__.py
+++ src/retinoto_py/__init__.py
@@ -4,6 +4,8 @@ __author__ = """Laurent U Perrinet"""
 __email__ = 'laurent.perrinet@cnrs.fr'
 
 
+import multiprocessing
+multiprocessing.set_start_method('fork', force=True)
 
 
 #####################################################
