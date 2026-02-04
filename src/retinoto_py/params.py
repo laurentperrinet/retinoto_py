271abf413105138cd6230dffd3ad94d952cbf208
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2026-02-04 18:40:05 +0100

    Update num_epochs increment and hyperparameters in Params
    - In 34_optimize-bbox.ipynb cell 10, change args_dict['num_epochs'] offset from +1 to +2 to extend training by an extra epoch.
    - In src/retinoto_py/params.py, increase base_lr and final_lr from 1e-5 and 1e-6 to 1e-4 and 1e-5 respectively, and raise augment_magnitude from 15 to 18.

--- src/retinoto_py/params.py
+++ src/retinoto_py/params.py
@@ -73,8 +73,8 @@ class Params:
     # loss_name: str = 'BCEWithLogitsLoss'
     # loss_name: str = 'NegLogitLoss'
     loss_name: str = "CrossEntropyLoss"
-    base_lr: float = 10.0e-6
-    final_lr: float = 1.0e-6
+    base_lr: float = 10.0e-5
+    final_lr: float = 1.0e-5
     num_warmup_epochs: int = 20
     delta1: float = 0.05
     delta2: float = 1e-2
@@ -82,7 +82,7 @@ class Params:
     label_smoothing: float = 0.05  # See https://docs.pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
     do_full_training: bool = True
     do_augment: bool = True
-    augment_magnitude: int = 15
+    augment_magnitude: int = 18
     augment_proba: float = 0.10
     # https://docs.pytorch.org/vision/main/generated/torchvision.ops.stochastic_depth.html#torchvision.ops.stochastic_depth
     stochastic_depth_prob: float = 0.40
