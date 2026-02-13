0e10c7638d948425f0af8e99b8d1336c35fa35d7
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2026-02-13 08:55:28 +0100

    Increase default augment magnitude to 21
    Raise augment_magnitude to strengthen data augmentation during training.

--- src/retinoto_py/params.py
+++ src/retinoto_py/params.py
@@ -82,7 +82,7 @@ class Params:
     label_smoothing: float = 0.01  # See https://docs.pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
     do_full_training: bool = True
     do_augment: bool = True
-    augment_magnitude: int = 18
+    augment_magnitude: int = 21
     augment_proba: float = 0.15
     # https://docs.pytorch.org/vision/main/generated/torchvision.ops.stochastic_depth.html#torchvision.ops.stochastic_depth
     stochastic_depth_prob: float = 0.7
