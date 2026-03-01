148422617d23a752e3b460ada57fe9770a0d2ffe
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2026-03-01 08:42:45 +0100

    Update hyperparameters in params.py
    Adjust delta1, delta2, and stochastic_depth_prob values to improve model training convergence.

--- src/retinoto_py/params.py
+++ src/retinoto_py/params.py
@@ -76,8 +76,8 @@ class Params:
     base_lr: float = 10.0e-6
     final_lr: float = 3.0e-8
     num_warmup_epochs: int = 20
-    delta1: float = 0.15
-    delta2: float = 0.15
+    delta1: float = 0.05
+    delta2: float = 0.01
     weight_decay: float = 0.02
     label_smoothing: float = 0.01  # See https://docs.pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
     do_full_training: bool = True
@@ -85,7 +85,7 @@ class Params:
     augment_magnitude: int = 19
     augment_proba: float = 0.15
     # https://docs.pytorch.org/vision/main/generated/torchvision.ops.stochastic_depth.html#torchvision.ops.stochastic_depth
-    stochastic_depth_prob: float = 0.7
+    stochastic_depth_prob: float = 0.5
 
     seed: int = 1998  # Set the seed for reproducibility
     shuffle: bool = True  # Whether to shuffle the data during training
