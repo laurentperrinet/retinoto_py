80294fcf6bd2368c81bd8e9e9af1a72643657fb7
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2026-04-08 07:57:46 +0200

    Remove Adagrad optimizer from get_optimizer function
    Adagrad configuration used incorrect betas parameter which is not supported[9D[K
    supported by PyTorch's Adagrad implementation.

--- src/retinoto_py/retinoto_py.py
+++ src/retinoto_py/retinoto_py.py
@@ -69,8 +69,6 @@ def get_optimizer(args, model):
         optimizer = torch.optim.SGD(model.parameters(),  momentum=1-args.delta1, dampening=1-args.delta2, **optim_dict)
     elif args.optimizer_name=='rmsprop': 
         optimizer = torch.optim.RMSprop(model.parameters(), momentum=1-args.delta1, alpha=1-args.delta2, **optim_dict)
-    elif args.optimizer_name=='adagrad': 
-        optimizer = torch.optim.Adagrad(model.parameters(), betas=(1-args.delta1, 1-args.delta2), **optim_dict)
     elif args.optimizer_name=='adadelta': 
         optimizer = torch.optim.Adadelta(model.parameters(), rho=1-args.delta1, **optim_dict)
     else:
