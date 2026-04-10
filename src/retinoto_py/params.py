11eff82d58c12154c96df44b1c3ce8bd9dc507a9
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2026-04-10 08:32:10 +0200

    Update hyperparameters in params.py
    Adjust learning rates, regularization, and augmentation parameters
    to improve model training performance.

--- src/retinoto_py/params.py
+++ src/retinoto_py/params.py
@@ -72,19 +72,19 @@ class Params:
     # loss_name: str = 'BCEWithLogitsLoss'
     # loss_name: str = 'NegLogitLoss'
     loss_name: str = "CrossEntropyLoss"
-    base_lr: float = 1.0e-6
-    final_lr: float = 3.0e-9
+    base_lr: float = 3.0e-6
+    final_lr: float = 10.0e-9
     num_warmup_epochs: int = 20
-    delta1: float = 0.05
-    delta2: float = 0.01
-    weight_decay: float = 0.001
-    label_smoothing: float = 0.002  # See https://docs.pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+    delta1: float = 0.02
+    delta2: float = 0.02
+    weight_decay: float = 0.0005
+    label_smoothing: float = 0.003  # See https://docs.pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
     do_full_training: bool = True
     do_augment: bool = True
     # augment_magnitude: int = 17
     augment_proba: float = 0.50
     # https://docs.pytorch.org/vision/main/generated/torchvision.ops.stochastic_depth.html#torchvision.ops.stochastic_depth
-    stochastic_depth_prob: float = 0.5
+    stochastic_depth_prob: float = 0.8
 
     seed: int = 1998  # Set the seed for reproducibility
     shuffle: bool = True  # Whether to shuffle the data during training
